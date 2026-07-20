ffea2a50b6f5c29a4e51d09cbe193249c7f395c4
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2026-07-20 09:36:28 -0700

    chore: Setup the docs-website branch to mirror Relay's setup (#84)
    #### Overview
    
    `docs-website` functions as a documentation snapshot repository detached from main and the rest of the code.
    
    #### Where should the reviewer start?
    
    * `README.md`
    * `.github/workflows/publish-fern-docs.yml`
    
    #### Related Issues: (use one of the action keywords Closes / Fixes / Resolves / Relates to)
    
    - PR #78
    
    - [x] I confirm this contribution is my own work, or I have the right to submit it under this project's license.
    - [x] I searched existing issues and open pull requests, and this does not duplicate existing work.
    
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - https://github.com/Salonijain27
      - Anuradha Karuppiah (https://github.com/AnuradhaKaruppiah)
    
    URL: https://github.com/NVIDIA/NeMo-Fabric/pull/84

--- python/src/nemo_fabric/integrations/harbor/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-"""Harbor integration for NeMo Fabric."""
-
-from nemo_fabric.integrations.harbor.fabric_agent import FabricAgent
-
-__all__ = ["FabricAgent"]
